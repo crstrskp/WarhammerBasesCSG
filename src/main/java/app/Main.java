71b694789af452ca4d81dd86818f626148f8ae1c
crstrskp <jesper@tjorne.dk>
2024-01-16 13:38:10 +0100

    wbc.setCsg and testing download

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -9,6 +9,8 @@ import io.javalin.rendering.template.JavalinThymeleaf;
 import org.abstractica.javacsg.JavaCSG;
 import org.abstractica.javacsg.JavaCSGFactory;
 
+import java.io.IOException;
+
 // Press Shift twice to open the Search Everywhere dialog
 public class Main {
     public static void main(String[] args) {
@@ -31,6 +33,7 @@ public class Main {
 
         BaseFactory bf = new BaseFactory(csg);
         MovementTrayFactory mtf = new MovementTrayFactory(csg);
+        WarhammerBaseController.setCsg(csg);
 
         UserValues.setBaseDiameter_mm(25.5);
         UserValues.setMovementTrayHeight_mm(5);
@@ -41,12 +44,19 @@ public class Main {
 
 //        csg.view(mtf.createRectangularMovementTrayWithRoundBases(2, 2));
 
-        var diameter = UserValues.getBaseDiameter_mm();
-        var height = UserValues.getBaseHeight_mm();
-        var chamfer = UserValues.getChamfer_mm();
-
-
-        csg.view(bf.buildCircularBase(diameter, height, chamfer));
+//        var diameter = UserValues.getBaseDiameter_mm();
+//        var height = UserValues.getBaseHeight_mm();
+//        var chamfer = UserValues.getChamfer_mm();
+//
+//        try {
+//            var shape = bf.buildCircularBase(diameter, height, chamfer);
+//
+//            var box = csg.box3D(10, 10, 10, false);
+////            csg.cache(shape);
+//            csg.saveSTL("OpenSCAD/output/shape.stl", shape);
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
 
 //        csg.saveSTL();
 //        csg.view(p1);
