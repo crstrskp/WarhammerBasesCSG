655574913d4e46ee13f2298944b3e7b046049b97
crstrskp <jesper@tjorne.dk>
2024-03-12 11:30:57 +0100

    Update Main.java

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -5,6 +5,7 @@ import app.Factories.MovementTrayFactory;
 import app.config.ThymeleafConfig;
 import app.controllers.FileController;
 import app.controllers.MovementTrayController;
+import app.controllers.MovementTraySquareController;
 import app.controllers.WarhammerBaseController;
 import io.javalin.Javalin;
 import io.javalin.rendering.template.JavalinThymeleaf;
@@ -21,6 +22,8 @@ public class Main {
         // routing
         WarhammerBaseController.addRoutes(app);
         MovementTrayController.addRoutes(app);
+        MovementTraySquareController.addRoutes(app);
+
         app.get("/", ctx -> ctx.render("index.html"));
 
         app.post("/", ctx -> UserValues.setDefaults(ctx));
@@ -32,23 +35,29 @@ public class Main {
         MovementTrayFactory mtf = new MovementTrayFactory(csg);
         FileController.setCsg(csg);
 
+
+        ////////////////////////////////////////////////////
+        /////////////////// TESTING CSG ////////////////////
+        ////////////////////////////////////////////////////
 //        JavaCSG csg = JavaCSGFactory.createDefault();
 //        BaseFactory bf = new BaseFactory(csg);
 //        MovementTrayFactory mtf = new MovementTrayFactory(csg);
 //
-//        UserValues.setBaseDiameter_mm(25.5);
+////        UserValues.setBaseDiameter_mm(25.5);
+//        UserValues.setBaseWidth_mm(30.5);
+//        UserValues.setBaseLength_mm(60.5);
 //        UserValues.setMovementTrayHeight_mm(5);
 //        UserValues.setBorderWidth_mm(.8);
-//        UserValues.setSpacing_mm(0);
+//        UserValues.setSpacing_mm(1);
 //        UserValues.setBaseHeight_mm(3.5);
 //        UserValues.setChamfer_mm(1.5);
 //        UserValues.setAddHoleForMagnet(true);
-//        UserValues.setMovementTrayX(3);
-//        UserValues.setMovementTrayY(5);
+////        UserValues.setMovementTrayX(3);
+////        UserValues.setMovementTrayY(5);
 //        UserValues.setMagnetDiameter(3.6);
 //        UserValues.setMagnetHoleHeight(1);
 //
-//        csg.view(mtf.createRectangularMovementTrayWithRoundBases(3, 5));
+//        csg.view(mtf.createRectangularMovementTray(1, 1));
 
     }
 }
